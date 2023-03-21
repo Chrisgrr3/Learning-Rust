28a42fe66b141c881fc47accb2f0e4e883e2e343
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-21 01:47:56 -0700

    Added additional print statement

--- hello.rs
+++ hello.rs
@@ -4,6 +4,7 @@ fn main() {
 
     // println! is a macro that prints text to the console
     println!("Hello World!")
+    println!("I'm a Rustacean!")
 }
 
 // Binary can be generated using rustc hello.rs
