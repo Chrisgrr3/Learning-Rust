6895aff2c7de44d999bece08e490710701834d47
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-21 01:45:26 -0700

    Provided comments for what each line of code means

--- hello.rs
+++ hello.rs
@@ -1,3 +1,9 @@
+// The main function
 fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // println! is a macro that prints text to the console
     println!("Hello World!")
 }
+
+// Binary can be generated using rustc hello.rs
